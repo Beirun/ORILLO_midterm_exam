36cd638ac4bdf3638ec25af5a265ab97ac60b9f2
Beirun <orillobernardjay@gmail.com>
2025-03-15 14:49:33 +0800

    Add question4.js

---
+++ question4.js
@@ -0,0 +1,24 @@
+var tasks = [];
+// add a new task
+function addTask(task) {
+    tasks.push(task);
+}
+// view all tasks
+function viewTasks() {
+    return tasks;
+}
+// update a task
+function updateTask(id, task) {
+    tasks[id] = task;
+}
+// delete a task
+function deleteTask(id) {
+    tasks.splice(id, 1);
+}
+// test the functions
+addTask({ id: 1, name: 'Task 1', description: 'Description 1' });
+console.log(viewTasks());
+updateTask(0, { id: 1, name: 'Task 2', description: 'Description 2' });
+console.log(viewTasks());
+deleteTask(0);
+console.log(viewTasks());
